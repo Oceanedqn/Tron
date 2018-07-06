2496f48dd13573f8ac8c99da26c8ba386686473d
Cox <Cox@DESKTOP-4EHEG5J.home>
2018-07-06 23:03:12 +0200

    Peut etre que c'est bon

---
+++ lorann/main/src/test/java/main/TestUnit.java
@@ -0,0 +1,20 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUnit {
+
+	@Test
+	public void test() {
+		/*int nombre1 = 2;
+		int nombre2 = 3;
+		
+		int result = addition(nombre1,nombre2);
+			
+		assertEquals(result, 5);*/
+		
+	}
+
+}
